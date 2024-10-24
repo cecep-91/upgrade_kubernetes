99ab275199c4e7dc951e56ba40ba6152a4973add
cyberforce <cyberforce@indonesiaindicator.com>
2024-10-24 13:37:22 +0700

    make check_pod_status.py library compatible for both python 2 & 3

--- library/check_pod_status.py
+++ library/check_pod_status.py
@@ -1,8 +1,18 @@
-#!/usr/bin/python
-
 from ansible.module_utils.basic import AnsibleModule
 import subprocess
 import time
+import sys
+
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
+
+try:
+    import six
+except ImportError:
+    six = None
 
 def run_module():
     module_args = dict(
@@ -31,28 +41,33 @@ def run_module():
         end_time = time.time() + timeout
         while time.time() < end_time:
             try:
-                check_pods_cmd = f"KUBECONFIG={kubeconfig} kubectl get pods -A -o wide | grep -P '{pod_name_regex}' | grep '{node_name}'"
-                all_pods = subprocess.Popen(check_pods_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+                check_pods_cmd = "KUBECONFIG={} kubectl get pods -A -o wide | grep -P '{}' | grep '{}'".format(kubeconfig, pod_name_regex, node_name)
+                all_pods = subprocess.Popen(check_pods_cmd, shell=True, stdout=subprocess.PIPE, stderr=DEVNULL)
                 skip = False
             except:
                 skip = True
             if not skip:
                 stdout, _ = all_pods.communicate()
-                pods = stdout.decode().strip().splitlines()
+
+                if sys.version_info[0] == 3:
+                    pods = stdout.decode().strip().splitlines()
+                else:
+                    pods = stdout.strip().splitlines()
+
                 running_pods = [pod for pod in pods if "Running" in pod.split()]
                 if running_pods:
-                    result['message'] = f"Pod(s) matching '{pod_name_regex}' on node '{node_name}' are in Running state."
+                    result['message'] = "Pod(s) matching '{}' on node '{}' are in Running state.".format(pod_name_regex, node_name)
                     break
             else:
-                result['message'] = f"No pods matching '{pod_name_regex}' found on node '{node_name}'. Considering as Running."
+                result['message'] = "No pods matching '{}' found on node '{}'. Considering as Running.".format(pod_name_regex, node_name)
                 break
             time.sleep(5)
 
         else:
-            module.fail_json(msg=f"Timeout exceeded: Pod(s) matching '{pod_name_regex}' on node '{node_name}' are not in Running state.", **result)
+            module.fail_json(msg="Timeout exceeded: Pod(s) matching '{}' on node '{}' are not in Running state.".format(pod_name_regex, node_name), **result)
 
     except Exception as e:
-        module.fail_json(msg=f"Failed to get pod status: {str(e)}", **result)
+        module.fail_json(msg="Failed to get pod status: {}".format(str(e)), **result)
 
     module.exit_json(**result)
 
